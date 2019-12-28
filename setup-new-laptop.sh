21a9a3f4108e2a45395ac08781476e89a90941ac
bob <bob@eellike.lan>
2019-12-28 18:07:20 -0500

    fix script

--- setup-new-laptop.sh
+++ setup-new-laptop.sh
@@ -24,16 +24,26 @@ if (($EUID != 0)); then
   exit
 fi
 
-
-
-# It's assumed here this is being run on a new laptop 
+# It's assumed here this is being run on a new laptop
 # from the user created during the first boot wizard
 
 # The goal of the following code is to perform the following
-# 
+#
 # 1. Setup a filevault user who is unable to login but is able to unlock the disk
 # 2. Remove the user created during first boot from filevault
-# 3. Create a limited user for day to day use. 
+# 3. Create a limited user for day to day use.
+
+CURRENTUSER="$(ls /Users | sed 's/Shared//g' | sed 's/\.localized//g')"
+echo "Current user is "$CURRENTUSER
+
+# It's assumed here this is being run on a new laptop
+# from the user created during the first boot wizard
+
+# The goal of the following code is to perform the following
+#
+# 1. Setup a filevault user who is unable to login but is able to unlock the disk
+# 2. Remove the user created during first boot from filevault
+# 3. Create a limited user for day to day use.
 
 PASSWORDMATCH="0"
 while [ $PASSWORDMATCH -eq 0 ]
@@ -54,11 +64,11 @@ echo "Enter username for limited user"
 read LIMITEDUSER
 
 sysadminctl -addUser $LIMITEDUSER -fullName $LIMITEDUSER
-sysadminctl -addUser filevault -fullName filevault -password "$PASSWORD1" -secureTokenOn 
+sysadminctl -addUser filevault -fullName filevault -password "$PASSWORD1" -secureTokenOn
 pwpolicy -u $LIMITEDUSER -setpolicy "newPasswordRequired=1"
 sysadminctl -guestAccount Off
 sysadminctl -secureTokenOn filevault -password - -adminUser $(logname) -adminPassword -
-fdesetup enable -user filevault 
+fdesetup enable -user filevault
 fdesetup remove -user alice
 defaults write /Library/Preferences/com.apple.loginwindow SHOWFULLNAME -bool false
 chsh -s /usr/bin/false filevault
@@ -75,9 +85,9 @@ cp ./randommacaddress.sh /usr/local/bin/randommacaddress.sh
 cp ./com.randommacaddress.plist /Library/LaunchDaemons/com.randommacaddress.plist
 launchctl load -w /Library/LaunchDaemons/com.randommacaddress.plist
 
-# Enable Firewall 
+# Enable Firewall
 
-defaults write /Library/Preferences/com.apple.alf globalstate -int 2   
+defaults write /Library/Preferences/com.apple.alf globalstate -int 2
 
 # Disable Spotlight
 mdutil -a -i off
