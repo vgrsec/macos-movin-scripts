2c54c00204a20f87fcddb9606bba66639d4c0d3e
bob <bob@eellike.lan>
2019-12-29 11:15:41 -0500

    Add more settings

--- setup-new-user.sh
+++ setup-new-user.sh
@@ -11,6 +11,7 @@
 # https://github.com/arrelid/preferences/blob/master/defaults.sh
 # https://github.com/sj26/home/blob/master/move-in-osx
 # https://github.com/joeyhoer/starter/blob/master/apps/safari.sh
+# https://gist.github.com/Tristor/d3c699d16f6c1bbeec8f4c9d647a1f24
 
 ### System Privacy
 
@@ -67,10 +68,10 @@ defaults write com.apple.Safari HistoryAgeInDaysLimit 1
 defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
 
 # Disable AutoFill
-defaults write com.apple.Safari AutoFillCreditCardData -bool true
-defaults write com.apple.Safari AutoFillFromAddressBook -bool true
-defaults write com.apple.Safari AutoFillPasswords -bool true
-defaults write com.apple.Safari AutoFillMiscellaneousForms -bool true
+defaults write ~/Library/Preferences/com.apple.Safari.plist AutoFillCreditCardData -bool false
+defaults write ~/Library/Preferences/com.apple.Safari.plist AutoFillFromAddressBook -bool false
+defaults write ~/Library/Preferences/com.apple.Safari.plist AutoFillPasswords -bool false
+defaults write ~/Library/Preferences/com.apple.Safari.plist AutoFillMiscellaneousForms -bool false
 
 # Allow websites to check if Apple Pay is set up
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2ApplePayCapabilityDisclosureAllowed -bool false
@@ -123,6 +124,9 @@ defaults write com.apple.finder ShowStatusBar -bool true
 
 # Show absolute path in Finder's title bar.
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# Show path bar
+defaults write com.apple.finder ShowPathbar -bool true
 
 # Keep folders on top when sorting by name
 defaults write com.apple.finder _FXSortFoldersFirst -bool true
@@ -178,7 +182,58 @@ defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 
 #Disable Spotlight Indexing
+
 # audo mdutil -a -i off
 echo " "
 echo "Disable spotlight indexing and add /Volumes to exclusion in settings"
 echo "Spotlight is now protected by SIP https://www.jamf.com/jamf-nation/discussions/29884/10-14-cannot-modify-spotlight-v100-volumeconfiguration-plist"
+
+# Disable SpotLight Suggestion Search
+defaults write com.apple.lookup.shared LookupSuggestionsDisabled -bool true
+
+# Disable Spotlight from indexing pesky places like the internet
+defaults write com.apple.Spotlight orderedItems -array \
+'{"enabled" = 1;"name" = "APPLICATIONS";}' \
+'{"enabled" = 1;"name" = "SYSTEM_PREFS";}' \
+'{"enabled" = 1;"name" = "DIRECTORIES";}' \
+'{"enabled" = 0;"name" = "PDF";}' \
+'{"enabled" = 0;"name" = "FONTS";}' \
+'{"enabled" = 0;"name" = "DOCUMENTS";}' \
+'{"enabled" = 0;"name" = "MESSAGES";}' \
+'{"enabled" = 0;"name" = "CONTACT";}' \
+'{"enabled" = 0;"name" = "EVENT_TODO";}' \
+'{"enabled" = 0;"name" = "IMAGES";}' \
+'{"enabled" = 0;"name" = "BOOKMARKS";}' \
+'{"enabled" = 0;"name" = "MUSIC";}' \
+'{"enabled" = 0;"name" = "MOVIES";}' \
+'{"enabled" = 0;"name" = "PRESENTATIONS";}' \
+'{"enabled" = 0;"name" = "SPREADSHEETS";}' \
+'{"enabled" = 0;"name" = "SOURCE";}' \
+'{"enabled" = 0;"name" = "MENU_DEFINITION";}' \
+'{"enabled" = 0;"name" = "MENU_OTHER";}' \
+'{"enabled" = 0;"name" = "MENU_CONVERSION";}' \
+'{"enabled" = 0;"name" = "MENU_EXPRESSION";}' \
+'{"enabled" = 0;"name" = "MENU_WEBSEARCH";}' \
+'{"enabled" = 0;"name" = "MENU_SPOTLIGHT_SUGGESTIONS";}'
+
+# Prevent Time Machine from prompting to use new hard drives as backup volume
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+# Disable press-and-hold for keys in favor of key repeat
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+# Set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 0
+# Disable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+# Disable the crash reporter
+defaults write com.apple.CrashReporter DialogType -string "none"
+# Check for software updates daily, not just once per week
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+# Disable smart quotes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+# Disable smart dashes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+# Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
